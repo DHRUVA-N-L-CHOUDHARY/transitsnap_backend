bb6d7a5e1eb7c35cf83557aa98e0e67366e047d6
DHRUVA-N-L-CHOUDHARY <lci2021014@iiitl.ac.in>
2024-09-15 12:34:24 +0530

    added cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors');
 const { connectToDatabase } = require("./config/db");
 const app = express();
 require("dotenv").config({ path: ".env" });
